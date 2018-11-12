4da4fac1c2c8d515bdc8d3ca8ab14484effbfb83
Pablo Andrade <pabloemilioandrade@gmail.com>
2018-11-11 19:01:06 -0600

    Style[X_Tax]: Compact getters and setters
    * Changed all getters and setters to be on a single line, grouping them by attribute

--- java/mtax/X_Tax.java
+++ java/mtax/X_Tax.java
@@ -4,7 +4,7 @@ import javax.persistence.Id;
 import javax.persistence.Version;
 
 public class X_Tax {
-    
+
     private Object id;
     private BigDecimal base;
     private String tax;
@@ -21,127 +21,51 @@ public class X_Tax {
     private Date updated;
     private String version;
 
-    public X_Tax() {
-    }
+    public X_Tax() {}
 
-    public Object getId() {
-        return id;
-    }
-
-    public void setId(Object id) {
-        this.id = id;
-    }
-
-    public BigDecimal getBase() {
-        return base;
-    }
-
-    public void setBase(BigDecimal base) {
-        this.base = base;
-    }
-
-    public String getTax() {
-        return tax;
-    }
-
-    public void setTax(String tax) {
-        this.tax = tax;
-    }
-
-    public String getTypeFactor() {
-        return typeFactor;
-    }
-
-    public void setTypeFactor(String typeFactor) {
-        this.typeFactor = typeFactor;
-    }
-
-    public BigDecimal getRate() {
-        return rate;
-    }
-
-    public void setRate(BigDecimal rate) {
-        this.rate = rate;
-    }
-
-    public BigDecimal getFee() {
-        return fee;
-    }
-
-    public void setFee(BigDecimal fee) {
-        this.fee = fee;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public boolean isTransferred() {
-        return transferred;
-    }
-
-    public void setTransferred(boolean transferred) {
-        this.transferred = transferred;
-    }
+    public Object getId() {return id;}
+    public void setId(Object id) {this.id = id;}
     
-    public boolean isLocal() {
-        return local;
-    }
+    public BigDecimal getBase() {return base;}
+    public void setBase(BigDecimal base) {this.base = base;}
+    
+    public String getTax() {return tax;}
+    public void setTax(String tax) {this.tax = tax;}
+    
+    public String getTypeFactor() {return typeFactor;}
+    public void setTypeFactor(String typeFactor) {this.typeFactor = typeFactor;}
+    
+    public BigDecimal getRate() {return rate;}
+    public void setRate(BigDecimal rate) {this.rate = rate;}
+    
+    public BigDecimal getFee() {return fee;}
+    public void setFee(BigDecimal fee) {this.fee = fee;}
+    
+    public BigDecimal getAmount() {return amount;}
+    public void setAmount(BigDecimal amount) {this.amount = amount;}
+    
+    public boolean isTransferred() {return transferred;}
+    public void setTransferred(boolean transferred) {this.transferred = transferred;}
+    
+    public boolean isLocal() {return local;}
+    public void setLocal(boolean local) {this.local = local;}
+    
+    public boolean isActive() {return active;}
+    public void setActive(boolean active) {this.active = active;}
+    
+    public String getCreatedBy() {return createdBy;}
+    public void setCreatedBy(String createdBy) {this.createdBy = createdBy;}
+    
+    public String getUpdatedBy() {return updatedBy;}
+    public void setUpdatedBy(String updatedBy) {this.updatedBy = updatedBy;}
+    
+    public Date getCreated() {return created;}
+    public void setCreated(Date created) {this.created = created;}
 
-    public void setLocal(boolean local) {
-        this.local = local;
-    }
+    public Date getUpdated() {return updated;}
+    public void setUpdated(Date updated) {this.updated = updated;}
 
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
-    public String getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public String getUpdatedBy() {
-        return updatedBy;
-    }
-
-    public void setUpdatedBy(String updatedBy) {
-        this.updatedBy = updatedBy;
-    }
-
-    public Date getCreated() {
-        return created;
-    }
-
-    public void setCreated(Date created) {
-        this.created = created;
-    }
-
-    public Date getUpdated() {
-        return updated;
-    }
-
-    public void setUpdated(Date updated) {
-        this.updated = updated;
-    }
-
-    public String getVersion() {
-        return version;
-    }
-
-    public void setVersion(String version) {
-        this.version = version;
-    }
+    public String getVersion() {return version;}
+    public void setVersion(String version) {this.version = version;}
     
 }
